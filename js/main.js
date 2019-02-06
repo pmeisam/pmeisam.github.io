89e6530941a3dd6be3b89484f9296c45629aaf5e
Meisam <pmeisam@ymail.com>
2019-02-05 22:37:12 -0800

    links update

--- js/main.js
+++ js/main.js
@@ -1,47 +1,24 @@
-console.log("up and running");
-var photos = [
-	{
-		rank : "before",
-		image : "images/before.png"
-	},
-	{
-		rank : "Meisam",
-		image : "images/meisam.jpeg"
-	},
-	{
-		rank : "after",
-		image : "images/after.png"
-	}
-];
-
-var cardsInPlay = [];
-
-var createBoard = function () {
-	for ( var i = 1; i <= photos.length; i++ ){
-		var cardElement = document.createElement("img");
-		cardElement.setAttribute ("src", "images/back.jpg");
-		cardElement.setAttribute ("data-id", i);
-		cardElement.addEventListener ("click", flipCard);
-		document.getElementById ("game-board").appendChild(cardElement);
-	}
-	
-}
-
-var checkForMatch = function () {
-	if ( cardsInPlay[0].rank === "Meisam" )
-		alert("You did it!");
-	else 
-		alert("Sorry try agian!");
-}
-
-var flipCard = function () {
-	var cardId = this.getAttribute("data-id");
-	console.log("User flipped" + photos[cardId].rank);
-	console.log(photos[cardId].image);
-	cardsInPlay.push(photos[cardId].rank);
-	this.setAttribute('src', photos[cardId].image);
-	if ( cardsInPlay.length === 1) {
-		checkForMatch();
-	}
-}
-createBoard();
+$(document).ready(function(){
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+  
+	  // Make sure this.hash has a value before overriding default behavior
+	  if (this.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+  
+		// Store hash
+		var hash = this.hash;
+  
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		$('html, body').animate({
+		  scrollTop: $(hash).offset().top
+		}, 800, function(){
+	 
+		  // Add hash (#) to URL when done scrolling (default click behavior)
+		  window.location.hash = hash;
+		});
+	  } // End if
+	});
+  });
